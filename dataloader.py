96e7fd06ed07326b8203ee18aec5cb901f8f0923
Xiaohui Chen <37996225+Xiaohui9607@users.noreply.github.com>
2020-12-17 10:08:58 -0500

    Add files via upload

---
+++ dataloader.py
@@ -0,0 +1,37 @@
+"""
+LOAD DATA from file.
+"""
+
+# pylint: disable=C0301,E1101,W0622,C0103,R0902,R0915
+
+##
+import os
+import torch
+from torchvision import transforms
+from torchvision.transforms import functional as F
+from torch.utils.data import DataLoader
+from torchvision.datasets import MNIST, CIFAR10, ImageFolder
+
+from dataloader.datasets import get_cifar_anomaly_dataset
+from dataloader.datasets import get_mnist_anomaly_dataset
+# from dataloader.kdd_dataset import get_loader
+
+class Data:
+    """ Dataloader containing train and valid sets.
+    """
+    def __init__(self, train, valid):
+        self.train = train
+        self.valid = valid
+
+##
+def load_data(opt):
+
+    train_ds = KDD_dataset(opt, mode='train')
+    valid_ds = KDD_dataset(opt, mode='test')
+
+    ## DATALOADER
+    train_dl = DataLoader(dataset=train_ds, batch_size=opt.batch_size, shuffle=True, drop_last=True)
+    valid_dl = DataLoader(dataset=valid_ds, batch_size=opt.batch_size, shuffle=False, drop_last=False)
+
+    return Data(train_dl, valid_dl)
+
